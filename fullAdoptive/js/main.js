07e0ecdc7ca1e4a700171e48e8ec4ccb0620185d
AboveZer0 <47446354+AboveZer0@users.noreply.github.com>
2020-04-12 15:15:26 +0300

    Add files via upload

--- fullAdoptive/js/main.js
+++ fullAdoptive/js/main.js
@@ -3,4 +3,12 @@ $(document).ready(function() {
     $(".header__burger,.header__menu").toggleClass("active");
     $("body").toggleClass("lock");
   });
+  $(".button__js").click(function(event) {
+      $(".overlay").toggleClass("overlay__js");
+      $("body").toggleClass("lock");
+  });
+  $(".close__modal").click(function(event) {
+      $(".overlay").toggleClass("overlay__js");
+      $("body").toggleClass("lock");
+  });
 });
