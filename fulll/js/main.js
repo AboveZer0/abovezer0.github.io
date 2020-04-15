4e93b1a75ee548b39132fef93b4e168b9bce866d
AboveZer0 <47446354+AboveZer0@users.noreply.github.com>
2020-04-15 16:04:16 +0300

    Add files via upload

--- fulll/js/main.js
+++ fulll/js/main.js
@@ -4,9 +4,6 @@ $(document).ready(function() {
     $("body").toggleClass("lock");
     });
   $(".form__title").click(function(event) {
-    $(".form").toggleClass("form__active");
-    });
-  $(".form__title").click(function(event) {
-    $(".form__title").toggleClass("form__act");
+    $(".form").slideToggle(300);
     });
 });
